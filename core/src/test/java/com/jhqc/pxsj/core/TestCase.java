6b4da6205493f20d6f3ec4ac9a4c436547ef06f0
yang zongyuan <yangzongyuan@pxsj.com>
2017-03-09 21:40:41 +0800

    修改测试

--- core/src/test/java/com/jhqc/pxsj/core/TestCase.java
+++ core/src/test/java/com/jhqc/pxsj/core/TestCase.java
@@ -3,6 +3,7 @@ package com.jhqc.pxsj.core;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jhqc.pxsj.core.query.predicate.Predicate;
 import com.jhqc.pxsj.core.query.root.Root;
 
 public class TestCase {
@@ -21,10 +22,13 @@ public class TestCase {
     @Test
     public void testSimple() {
         Root<Goods> root = builder.root(Goods.class);
-//        
-//        builder.createQuery(Integer.class).select(root.getAttribute(Goods_.goodsId))
-//                                          .from(root)
-//                                          .where(root.getAttribute(meta))
+        
+        Predicate predicate = builder.predicate().and(root.get(Goods_.goodsId).equal(Integer.valueOf(1)))
+                                                 .and(root.get(Goods_.price).lt(Integer.valueOf(100)));
+        
+        builder.createQuery(Integer.class).select(root.get(Goods_.goodsId), root.get(Goods_.name))
+                                          .from(root)
+                                          .where(predicate);
     }
     
     @Test
